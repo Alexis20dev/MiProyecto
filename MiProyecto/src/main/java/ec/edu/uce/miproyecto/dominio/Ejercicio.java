10aacea7fab65f562376cd808428e3bcac7ff3af
RonaldAlexander <ronaldsupecango@gmail.com>
2026-05-09 10:45:14 -0500

    Update Ejercicio.java

--- MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Ejercicio.java
+++ MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Ejercicio.java
@@ -1,42 +1,66 @@
-package ec.edu.uce.miproyecto.dominio;
-import java.util.ArrayList;
-import java.util.List;
+package Dominio;
 
-public class Ejercicio {
+public class ejercicio {
 
-    private String enunciado;
-    private double respuestaCorrecta;
-    private int dificultad;
-    private Tema tema;
-    private Concepto concepto;
-    private List<Pista> pistas;
-    public Ejercicio(String enunciado, double respuestaCorrecta, int dificultad, Tema tema, Concepto concepto) {
-        this.enunciado = enunciado;
-        this.respuestaCorrecta = respuestaCorrecta;
+    private int idEjercicio;
+    private String enuncido;
+    private String respuesta;
+    private String dificultad;
+
+
+    public int getIdEjercicio() {
+        return idEjercicio;
+    }
+
+    public void setIdEjercicio(int idEjercicio) {
+        this.idEjercicio = idEjercicio;
+    }
+
+
+    public String getEnuncido() {
+        return enuncido;
+    }
+
+    public void setEnuncido(String enuncido) {
+        this.enuncido = enuncido;
+    }
+
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+
+    public String getDificultad() {
+        return dificultad;
+    }
+
+    public void setDificultad(String dificultad) {
         this.dificultad = dificultad;
-        this.tema = tema;
-        this.concepto = concepto;
-        pistas = new ArrayList<>();
     }
-    public boolean validarRespuesta(double respuesta) {
-        return respuesta == respuestaCorrecta;
+
+    public ejercicio() {
     }
-    public String mostrarEjercicio() {
-        return enunciado;
+
+    public ejercicio(int idEjercicio, String enuncido, String respuesta, String dificultad) {
+        this.idEjercicio = idEjercicio;
+        this.enuncido = enuncido;
+        this.respuesta = respuesta;
+        this.dificultad = dificultad;
     }
-    public void agregarPista(Pista pista) {
-        pistas.add(pista);
-    }
-    public Pista mostrarPista() {
-        if (pistas.isEmpty()) {
-            return null;
-        }
-        return pistas.get(0);
-    }
-    public Tema getTema() {
-        return tema;
-    }
-    public Concepto getConcepto() {
-        return concepto;
+
+    @Override
+    public String toString() {
+        return "ejercicio{" +
+                "idEjercicio=" + idEjercicio +
+                ", enuncido='" + enuncido + '\'' +
+                ", respuesta='" + respuesta + '\'' +
+                ", dificultad='" + dificultad + '\'' +
+                '}';
     }
 }
+
