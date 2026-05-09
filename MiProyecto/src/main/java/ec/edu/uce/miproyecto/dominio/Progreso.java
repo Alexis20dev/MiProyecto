365993f6f240a442585320bbee5f42b37d5337bc
RonaldAlexander <ronaldsupecango@gmail.com>
2026-05-09 10:49:33 -0500

    Update Progreso.java

--- MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Progreso.java
+++ MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Progreso.java
@@ -1,30 +1,74 @@
-package ec.edu.uce.miproyecto.dominio;
+package Dominio;
 
-public class Progreso {
-    private int ejerciciosResueltos;
-    private int ejerciciosCorrectos;
-    public Progreso() {
-        ejerciciosResueltos = 0;
-        ejerciciosCorrectos = 0;
-    }
-    public void actualizarProgreso(boolean correcto) {
-        ejerciciosResueltos++;
-        if (correcto) {
-            ejerciciosCorrectos++;
-        }
-    }
-    public double calcularPromedio() {
-        if (ejerciciosResueltos == 0) {
-            return 0;
-        }
-        return (double) ejerciciosCorrectos
-                / ejerciciosResueltos * 100;
-    }
-    public String mostrarEstadisticas() {
+import java.util.Date;
 
-        return "Resueltos: "
-                + ejerciciosResueltos
-                + " | Correctos: "
-                + ejerciciosCorrectos;
+public class progreso {
+    private int idProgreso;
+    private String estado;
+    private Date fecha;
+    private int puntaje;
+    private int tiempo;
+
+
+    public int getIdProgreso() {
+        return idProgreso;
+    }
+
+    public void setIdProgreso(int idProgreso) {
+        this.idProgreso = idProgreso;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public progreso() {
+    }
+
+    public progreso(int idProgreso, String estado, Date fecha, int puntaje, int tiempo) {
+        this.idProgreso = idProgreso;
+        this.estado = estado;
+        this.fecha = fecha;
+        this.puntaje = puntaje;
+        this.tiempo = tiempo;
+    }
+
+    @Override
+    public String toString() {
+        return "progreso{" +
+                "idProgreso=" + idProgreso +
+                ", estado='" + estado + '\'' +
+                ", fecha=" + fecha +
+                ", puntaje=" + puntaje +
+                ", tiempo=" + tiempo +
+                '}';
     }
 }
