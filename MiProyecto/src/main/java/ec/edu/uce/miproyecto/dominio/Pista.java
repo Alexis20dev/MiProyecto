c6556ef16260ae13a3bfd2c68985cb6f7c35a724
jesato2019-dev <jesato2019@gmail.com>
2026-05-09 10:50:40 -0500

    Update Pista.java

--- MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Pista.java
+++ MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Pista.java
@@ -1,16 +1,49 @@
 package ec.edu.uce.miproyecto.dominio;
 
-public class Pista {
+public class Pistas {
+    private int idpista;
     private String descripcion;
-    private int nivelAyuda;
-    public Pista(String descripcion, int nivelAyuda) {
-        this.descripcion = descripcion;
-        this.nivelAyuda = nivelAyuda;
+    private int orden;
+
+    public int getIdpista() {
+        return idpista;
     }
-    public String mostrarPista() {
+
+    public void setIdpista(int idpista) {
+        this.idpista = idpista;
+    }
+
+    public String getDescripcion() {
         return descripcion;
     }
-    public int getNivelAyuda() {
-        return nivelAyuda;
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getOrden() {
+        return orden;
+    }
+
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
+
+    public Pistas() {
+    }
+
+    public Pistas(int idpista, String descripcion, int orden) {
+        this.idpista = idpista;
+        this.descripcion = descripcion;
+        this.orden = orden;
+    }
+
+    @Override
+    public String toString() {
+        return "Pistas{" +
+                "idpista=" + idpista +
+                ", descripcion='" + descripcion + '\'' +
+                ", orden=" + orden +
+                '}';
     }
 }
