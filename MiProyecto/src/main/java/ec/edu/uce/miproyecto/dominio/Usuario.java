39f23813eb2bff7b3f569348c8bce0eb3f049cf5
jesato2019-dev <jesato2019@gmail.com>
2026-05-09 10:44:45 -0500

    Update Usuario.java
    editado por jeremy

--- MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Usuario.java
+++ MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Usuario.java
@@ -1,32 +1,82 @@
 package ec.edu.uce.miproyecto.dominio;
-
+import java.util.Date;
 public class Usuario {
 
-    protected String username;
-    protected String password;
-    protected String nombre;
-    // CONSTRUCTOR
-    public Usuario(String username, String password, String nombre){
-        this.username = username;
-        this.password = password;
-        this.nombre = nombre;
+    private int idUsuario;
+    private String nombre;
+    private String email;
+    private String contrasena;
+    private Date fechaRegistro;
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "idUsuario=" + idUsuario +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", contrasena='" + contrasena + '\'' +
+                ", fechaRegistro=" + fechaRegistro +
+                ", inicioSesion=" + inicioSesion +
+                '}';
     }
-    public boolean iniciarSesion(String user, String pass){
-        return username.equals(user)
-                &&
-                password.equals(pass);
+
+    private InicioSesion inicioSesion;
+
+    public InicioSesion getInicioSesion() {
+        return inicioSesion;
     }
-    public void cerrarSesion() {
-        System.out.println("Sesión cerrada");
+
+    public void setInicioSesion(InicioSesion inicioSesion) {
+       this.inicioSesion = inicioSesion;
     }
-    public void cambiarPassword(String nuevaPassword) {
-        this.password = nuevaPassword;
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
     }
-    public String getUsername() {
-        return username;
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
     }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getNombre() {
         return nombre;
     }
-}
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    public Usuario() {
+    }
+    public Usuario(String email, String contrasena, Date fechaRegistro, InicioSesion inicioSesion, String nombre, int idUsuario){
+        this.email = email;
+        this.contrasena = contrasena;
+        this.fechaRegistro = fechaRegistro;
+       this.inicioSesion = inicioSesion;
+        this.nombre = nombre;
+        this.idUsuario = idUsuario;
+    }
+}
