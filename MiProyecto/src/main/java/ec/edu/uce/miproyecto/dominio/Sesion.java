6680fd0e861f8791a6992e86e639f93b8fcbe6c7
jesato2019-dev <jesato2019@gmail.com>
2026-05-09 10:48:06 -0500

    Update Sesion.java
    editado por jeremy

--- MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Sesion.java
+++ MiProyecto/src/main/java/ec/edu/uce/miproyecto/dominio/Sesion.java
@@ -1,23 +1,62 @@
 package ec.edu.uce.miproyecto.dominio;
 
-public class Sesion {
-    private Usuario usuarioActivo;
-    public boolean validarCredenciales(Usuario usuario, String username, String password) {
-        return usuario.iniciarSesion(username, password);
+import java.util.Date;
+
+public class InicioSesion {
+    private int idSesion;
+    private Date fechainicio;
+    private Date fechafin;
+    private String estado;
+
+    public Date getFechainicio() {
+        return fechainicio;
     }
-    public void iniciarSesion(Usuario usuario) {
-        usuarioActivo = usuario;
-        System.out.println(
-                "Sesión iniciada"
-        );
+
+    public void setFechainicio(Date fechainicio) {
+        this.fechainicio = fechainicio;
     }
-    public void cerrarSesion() {
-        usuarioActivo = null;
-        System.out.println(
-                "Sesión cerrada"
-        );
+
+    public Date getFechafin() {
+        return fechafin;
     }
-    public Usuario getUsuarioActivo() {
-        return usuarioActivo;
+
+    public void setFechafin(Date fechafin) {
+        this.fechafin = fechafin;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getIdSesion() {
+        return idSesion;
+    }
+
+    public void setIdSesion(int idSesion) {
+        this.idSesion = idSesion;
+    }
+
+    public InicioSesion() {
+    }
+
+    @Override
+    public String toString() {
+        return "InicioSesion{" +
+                "idSesion=" + idSesion +
+                ", fechainicio=" + fechainicio +
+                ", fechafin=" + fechafin +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+
+    public InicioSesion(Date fechainicio, Date fechafin, String estado, int idSesion) {
+        this.fechainicio = fechainicio;
+        this.fechafin = fechafin;
+        this.estado = estado;
+        this.idSesion = idSesion;
     }
 }
