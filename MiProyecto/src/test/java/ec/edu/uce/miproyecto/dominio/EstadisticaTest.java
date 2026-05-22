b70035c3ced88eb17045035049df19dcac573a2c
Alexis20dev <roberth.carvajal13@gmail.com>
2026-05-21 19:34:22 -0500

    Añado clase test de estadistica

---
+++ MiProyecto/src/test/java/ec/edu/uce/miproyecto/dominio/EstadisticaTest.java
@@ -0,0 +1,52 @@
+package ec.edu.uce.miproyecto.dominio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstadisticaTest {
+
+    @Test
+    void getIdEstadistica() {
+    }
+
+    @Test
+    void setIdEstadistica() {
+    }
+
+    @Test
+    void getAciertos() {
+    }
+
+    @Test
+    void setAciertos() {
+    }
+
+    @Test
+    void getFallos() {
+    }
+
+    @Test
+    void setFallos() {
+    }
+
+    @Test
+    void getPorcentaje() {
+    }
+
+    @Test
+    void setPorcentaje() {
+    }
+
+    @Test
+    void getNivel() {
+    }
+
+    @Test
+    void setNivel() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
